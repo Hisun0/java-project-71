96d63f642dc68d821d9fa8e77c0873edc23d7c9c
Hisun0 <tridvad1@gmail.com>
2024-01-25 17:46:50 +0300

    4 step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,15 +3,25 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
     description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = { "-f", "--format" }, description = "output format [default: stylish]")
+    private String format;
+
     @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
-    boolean helpRequired = false;
+    private boolean helpRequired;
 
     @Option(names = { "-V", "--version" }, versionHelp = true, description = "Print version information and exit.")
-    boolean versionRequested;
+    private boolean versionRequested;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
