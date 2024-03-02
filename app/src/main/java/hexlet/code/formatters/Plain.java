2f03339627a3c24c3c811b15a41a51c7712f7c22
Hisun0 <tridvad1@gmail.com>
2024-03-02 15:05:08 +0300

    lint fix

--- app/src/main/java/hexlet/code/formatters/Plain.java
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -9,8 +9,8 @@ public class Plain {
     private static final String CHANGED = "updated";
 
     private static boolean isComplexValue(String value) {
-        return value.startsWith("{") && value.endsWith("}") ||
-                value.startsWith("[") && value.endsWith("]");
+        return value.startsWith("{") && value.endsWith("}")
+                || value.startsWith("[") && value.endsWith("]");
     }
 
     private static boolean isBoolean(String value) {
@@ -20,7 +20,7 @@ public class Plain {
     private static boolean isNull(String value) {
         return value.equals("null");
     }
-    
+
     private static boolean isInteger(String value) {
         try {
             Integer.parseInt(value);
@@ -29,16 +29,16 @@ public class Plain {
             return false;
         }
     }
-    
+
     private static String getFormattedValue(String value) {
         if (isComplexValue(value)) {
             return "[complex value]";
         }
-        
+
         if (isBoolean(value) || isInteger(value) || isNull(value)) {
             return value;
         }
-        
+
         return "'" + value + "'";
     }
 
@@ -50,36 +50,26 @@ public class Plain {
             String value = String.valueOf(obj.get("value"));
 
             switch (obj.get("status").toString()) {
-                case "deleted": {
-                    String diffString = String.format(
+                case "deleted":
+                    stringBuilder.append(String.format(
                             "Property '%1$s' was %2$s",
                             key, DELETED
-                    );
-
-                    stringBuilder.append(diffString).append("\n");
+                    )).append("\n");
                     break;
-                }
-                case "added": {
-                    String diffString = String.format(
+                case "added":
+                    stringBuilder.append(String.format(
                             "Property '%1$s' was %2$s with value: %3$s",
                             key, ADDED, getFormattedValue(value)
-                    );
-
-                    stringBuilder.append(diffString).append("\n");
+                    )).append("\n");
                     break;
-                }
-                case "changed": {
-                    String oldValue = String.valueOf(obj.get("oldValue"));
-                    String newValue = String.valueOf(obj.get("newValue"));
-                    
-                    String diffString = String.format(
+                case "changed":
+                    stringBuilder.append(String.format(
                             "Property '%1$s' was %2$s. From %3$s to %4$s",
-                            key, CHANGED, getFormattedValue(oldValue), getFormattedValue(newValue)
-                    );
-
-                    stringBuilder.append(diffString).append("\n");
+                            key, CHANGED,
+                            getFormattedValue(String.valueOf(obj.get("oldValue"))),
+                            getFormattedValue(String.valueOf(obj.get("newValue")))
+                    )).append("\n");
                     break;
-                }
                 default:
                     break;
             }
